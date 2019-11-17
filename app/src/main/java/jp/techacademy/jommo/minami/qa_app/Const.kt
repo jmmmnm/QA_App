b871517a350ce614d4ec11f3ffbd4cf333f3cb1d
jmmmnm <jmmmnm@gmail.com>
2019-11-17 17:50:44 +0900

    11.1 お気に入りのパスを設定

--- app/src/main/java/jp/techacademy/jommo/minami/qa_app/Const.kt
+++ app/src/main/java/jp/techacademy/jommo/minami/qa_app/Const.kt
@@ -4,3 +4,4 @@ const val UsersPATH = "users"       // Firebaseにユーザーの表示名を保
 const val ContentsPATH = "contents"  // Firebaseに質問を保存するパス
 const val AnswersPATH = "answers"   // Firebaseに解答を保存するパス
 const val NameKEY = "name"          // Preferenceに表示名を保存する時のキー
+const val Favorite = "favorite"     // Firebaseにお気に入りを保存するパス
